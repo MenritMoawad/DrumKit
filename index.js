acd3f2d2e1aeebd46e02f8f14cdae68e8697e6a1
menritmoawad <menrit.moawad@gmail.com>
2021-04-08 14:22:29 +0200

    first commit add files

---
+++ index.js
@@ -0,0 +1,10 @@
+var numOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+
+    });
+    
+}
